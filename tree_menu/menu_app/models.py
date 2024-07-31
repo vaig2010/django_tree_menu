7de68751208dd51407e170af261d6046e1e561fd
Игорь <vaskinin97@gmail.com>
2024-07-31 19:12:23 +0400

    new class menu

--- tree_menu/menu_app/models.py
+++ tree_menu/menu_app/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-class Menu(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    url = models.CharField(max_length=100, blank=True, null=True)
+    named_url = models.CharField(max_length=100, blank=True, null=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+    menu_name = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        if self.named_url:
+            return reverse(self.named_url)
+        return self.url
 
     def __str__(self):
         return self.name
-    
-    class Meta:
-        verbose_name = 'Menu'
-        verbose_name_plural = 'Menus'
-    
-    
