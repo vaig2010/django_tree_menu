f8c2b01abbfd9397f68a247c417ed00fb8ace161
Игорь <vaskinin97@gmail.com>
2024-08-01 17:13:23 +0400

    added all views

--- tree_menu/menu_app/views.py
+++ tree_menu/menu_app/views.py
@@ -1,3 +1,34 @@
+# app/views.py
 from django.shortcuts import render
+from .models import Menu
 
-# Create your views here.
+def index(request):
+    return render(request, 'menu_app/index.html', {'menus': Menu.objects.all()})
+
+def draw_menu(request, path):
+    splitted_path = path.split('/')
+    return render(request, 'menu_app/index.html', {'menu_name': splitted_path[0]})
+
+def home(request):
+    context = {
+        'title': 'Home',
+    }
+    return render(request, 'menu_app/home.html', context)
+
+def about(request):
+    return render(request, 'menu_app/about.html')
+
+def web_design(request):
+    return render(request, 'menu_app/web_design.html')
+
+def seo(request):
+    return render(request, 'menu_app/seo.html')
+
+def marketing(request):
+    return render(request, 'menu_app/marketing.html')
+
+def contact(request):
+    return render(request, 'menu_app/contact.html')
+
+def privacy_policy(request):
+    return render(request, 'menu_app/privacy_policy.html')
